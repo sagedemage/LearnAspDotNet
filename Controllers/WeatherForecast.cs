5f17920624c3051c5d8f2e9ddc474b8a6c95ec39
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-06 18:19:06 -0400

    Add project files.

---
+++ Controllers/WeatherForecast.cs
@@ -0,0 +1,31 @@
+﻿namespace LearnAspDotNet.Controllers
+{
+    public class WeatherForecast
+    {
+        private System.DateTime Date;
+        private int TemperatureC;
+        private string? Summary;
+
+        public WeatherForecast(System.DateTime Date, int TemperatureC, string Summary)
+        {
+            this.Date = Date;
+            this.TemperatureC = TemperatureC;
+            this.Summary = Summary;
+        }
+
+        public System.DateTime getDate()
+        {
+            return this.Date;
+        }
+
+        public int getTemperatureC() 
+        { 
+            return this.TemperatureC; 
+        }
+
+        public string getSummary() {
+            return this.Summary;
+        }
+
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnAspDotNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public string[] Get()
+        {
+            return Summaries;
+        }
+    }
+}
