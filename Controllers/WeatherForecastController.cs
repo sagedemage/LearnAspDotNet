00846765556ce45b9bb1b90c70181ec441595be8
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-07 23:13:22 -0400

    reorder the apis based on crud

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,15 +19,6 @@ namespace LearnAspDotNet.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public JsonResult Get()
-        {
-            /* READ */
-            var data = new { status = "Cold", message="It is cold outside." };
-            var json = new JsonResult(data);
-            return json;
-        }
-
         [HttpPost(Name = "PostWeatherForecast")]
         public JsonResult Post(WeatherBody weatherBody)
         {
@@ -37,21 +28,30 @@ namespace LearnAspDotNet.Controllers
             return json;
         }
 
-        [HttpDelete(Name = "DeleteWeatherForecast")]
-        public JsonResult Delete(long id)
+        [HttpGet(Name = "GetWeatherForecast")]
+        public JsonResult Get()
         {
-            /* DELETE */
-            var data = new { id = id, message = "Deleted weather forecast" };
+            /* READ */
+            var data = new { status = "Cold", message="It is cold outside." };
             var json = new JsonResult(data);
             return json;
         }
 
-        [HttpPatch(Name ="PatchWeatherForecast")]
+        [HttpPatch(Name = "PatchWeatherForecast")]
         public JsonResult Patch(WeatherBody weatherBody)
         {
             /* UPDATE */
             var weather_data = new { id = weatherBody.id, status = weatherBody.Status, message = weatherBody.Message };
             var data = new { body = weather_data, message = "Updated weather forecast" };
+            var json = new JsonResult(data);
+            return json;
+        }
+
+        [HttpDelete(Name = "DeleteWeatherForecast")]
+        public JsonResult Delete(long id)
+        {
+            /* DELETE */
+            var data = new { id = id, message = "Deleted weather forecast" };
             var json = new JsonResult(data);
             return json;
         }
