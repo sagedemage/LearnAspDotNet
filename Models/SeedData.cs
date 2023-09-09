9f2bbacb4fa81e1e4b4cfcb80bff59f0cfbfb8c5
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-09 01:09:30 -0400

    the program can seed the database with data

---
+++ Models/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+// Issue is right here
+using Microsoft.Extensions.DependencyInjection;
+using LearnAspDotNet.Data;
+using System;
+using System.Linq;
+
+namespace LearnAspDotNet.Models
+{
+    public class SeedData
+    {
+        // cannot convert from 'System.IServiceProvider' to 'Microsoft.Extensions.DependencyInjection.IServiceCollection'
+        // System.IServiceProvider
+        // Microsoft.Extensions.DependencyInjection.IServiceCollection
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WeatherContext(
+                serviceProvider.GetRequiredService<DbContextOptions<WeatherContext>>()))
+            {
+                if (context.Weather.Any())
+                {
+                    return; // DB has been seeded
+                }
+                context.Weather.AddRange(
+                    new Weather
+                    {
+                        Status = "Cold",
+                        Message = "It is cold outside!"
+                    },
+                    new Weather
+                    {
+                        Status = "Hot",
+                        Message = "It is hot outside!"
+                    },
+                    new Weather
+                    {
+                        Status = "Humid",
+                        Message = "It is humid outside!"
+                    },
+                    new Weather
+                    {
+                        Status = "Dry",
+                        Message = "It is dry outside!"
+                    }
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+
 using LearnAspDotNet.Data;
+using LearnAspDotNet.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +14,13 @@ builder.Services.AddDbContext<WeatherContext>(options =>
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
