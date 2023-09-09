7e5a83d04ae4cc063920c6afbd700889936991c1
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-08 20:35:33 -0400

    created weather context

---
+++ Context/WeatherContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LearnAspDotNet.Models;
+
+namespace LearnAspDotNet.Context
+{
+        public class WeatherContext : DbContext
+        {
+            public WeatherContext(DbContextOptions<WeatherContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Weather> Weather { get; set; }
+    }
+}
