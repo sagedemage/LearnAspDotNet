07529f32413707fd4c85e520a91a6e80db7ea949
Salmaan Saeed <salsaeed999@gmail.com>
2023-09-26 23:11:56 -0400

    the edit form can update a weather entry during submission

--- wwwroot/js/edit_weather_onload.js
+++ wwwroot/js/edit_weather_onload.js
@@ -3,9 +3,9 @@
 window.onload = (event) => {
     const params = new URLSearchParams(window.location.search)
     const id = params.get('id')
+    document.getElementById("weather_id").value = id
 
-    const response = axios.get("Fetch?id=" + id).then(function (response) {
-        console.log(response.data)
+    axios.get("Fetch?id=" + id).then(function (response) {
         document.getElementById("status").value = response.data.status
         document.getElementById("message").value = response.data.message
     }).catch(function (error) {
